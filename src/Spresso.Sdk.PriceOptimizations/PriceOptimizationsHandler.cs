868309d03b53c5cb4ad4c9bddd3122225472d729
Natalia Taylor <natalia@boxed.com>
2023-09-27 20:15:14 -0400

    remove retry strategy and circuit breaker set http timeout default to 1 sec

--- src/Spresso.Sdk.PriceOptimizations/PriceOptimizationsHandler.cs
+++ src/Spresso.Sdk.PriceOptimizations/PriceOptimizationsHandler.cs
@@ -459,25 +459,9 @@ namespace Spresso.Sdk.PriceOptimizations
         {
             var retryErrors = new[] { PriceOptimizationError.Timeout, PriceOptimizationError.Unknown };
             return ResiliencyPolicyBuilder.BuildPolicy(
-                new RetryOptions<T>(r => !r.IsSuccess && retryErrors.Contains(r.Error), options.NumberOfRetries),
                 new TimeoutOptions(options.Timeout),
-                new CircuitBreakerOptions<T>(
-                    r => !r.IsSuccess && retryErrors.Contains(r.Error),
-                    options.NumberOfFailuresBeforeTrippingCircuitBreaker,
-                    options.CircuitBreakerBreakDuration,
-                    (response, timespan, context) =>
-                    {
-                        _logger.LogWarning(
-                            "@@{0}.{1}@@ Circuit breaker tripped for {2}ms due to error {3}.  Exception details (if applicable): {4}: ",
-                            nameof(PriceOptimizationsHandler), caller, timespan.TotalMilliseconds,
-                            response.Result.Error, response.Exception?.Message);
-                    },
-                    context =>
-                    {
-                        _logger.LogInformation("@@{0}.{1}@@ Circuit breaker reset", nameof(PriceOptimizationsHandler),
-                            caller);
-                    }),
-                fallbackOptions);
+                fallbackOptions
+            );
         }
 
         private PriceOptimization CreatePriceOptimization(string priceOptimizationJson)
